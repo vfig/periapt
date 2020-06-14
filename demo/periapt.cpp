083724b7c199edcc9c0b3dd5b64fc04c878d6080
Andy Durdin <me@andy.durdin.net>
2020-06-14 02:15:35 +0100

    Move stuff around and tidy up. Show the console in a nice place.

--- demo/periapt.cpp
+++ demo/periapt.cpp
@@ -33,6 +33,148 @@
 
 using namespace std;
 
+/*** Exe identity and information ***/
+
+#define ExeSignatureBytesMax 32
+#define ExeSignatureFixupsMax 8
+enum ExeIdentity {
+    ExeThief_v126 = 0,
+    ExeDromEd_v126 = 1,
+    ExeThief_v127 = 2,
+    ExeDromEd_v127 = 3,
+
+    ExeIdentityCount,
+    ExeIdentityUnknown = -1,
+};
+struct ExeSignature {
+    DWORD offset;
+    DWORD size;
+    UCHAR bytes[ExeSignatureBytesMax];
+    CHAR fixups[ExeSignatureFixupsMax];
+};
+struct ExeFunctionPointers {
+    DWORD cam_render_scene;
+};
+struct ExeInfo {
+    const char *name;
+    const char *version;
+    ExeSignature signature;
+    ExeFunctionPointers functions;
+};
+static const ExeInfo ExeInfoTable[ExeIdentityCount] = {
+    {
+        "Thief2", "ND 1.26",
+        {   // Signature
+            0x001bc7a0UL, 31,
+            {
+            0x83,0xec,0x28,0xdd,0x05,0xd8,0xde,0x38,
+            0x00,0x53,0x56,0xdd,0x54,0x24,0x24,0xdd,
+            0x05,0xe0,0xde,0x38,0x00,0x8b,0xf0,0x8b,
+            0x06,0xdd,0x54,0x24,0x1c,0xd9,0x05,
+            },
+            { 5, 17, -1 },
+        },
+        {   // FunctionPointers
+            0x001bc7a0UL, // cam_render_scene
+        },
+    },
+    {
+        "DromEd", "ND 1.26",
+        {   // Signature
+            0x00286960UL, 31,
+            {
+                0x83,0xec,0x28,0xdd,0x05,0xa8,0xdf,0x4a,
+                0x00,0x53,0x56,0xdd,0x54,0x24,0x24,0xdd,
+                0x05,0xb0,0xdf,0x4a,0x00,0x8b,0xf0,0x8b,
+                0x06,0xdd,0x54,0x24,0x1c,0xd9,0x05,
+            },
+            { 5, 17, -1 },
+        },
+        {   // FunctionPointers
+            0x00286960UL, // cam_render_scene
+        },
+    },
+    {
+        "Thief2", "ND 1.27",
+        {   // Signature
+            0x001bd820UL, 31,
+            {
+                0x83,0xec,0x28,0xdd,0x05,0xd8,0xee,0x38,
+                0x00,0x53,0x56,0xdd,0x54,0x24,0x24,0xdd,
+                0x05,0xe0,0xee,0x38,0x00,0x8b,0xf0,0x8b,
+                0x06,0xdd,0x54,0x24,0x1c,0xd9,0x05,
+            },
+            { 5, 17, -1 },
+        },
+        {   // FunctionPointers
+            0x001bd820UL, // cam_render_scene
+        },
+    },
+    {
+        "DromEd", "ND 1.27",
+        {   // Signature
+            0x002895c0UL, 31,
+            {
+                0x83,0xec,0x28,0xdd,0x05,0xa8,0x1f,0x4b,
+                0x00,0x53,0x56,0xdd,0x54,0x24,0x24,0xdd,
+                0x05,0xb0,0x1f,0x4b,0x00,0x8b,0xf0,0x8b,
+                0x06,0xdd,0x54,0x24,0x1c,0xd9,0x05,
+            },
+            { 5, 17, -1 },
+        },
+        {   // FunctionPointers
+            0x002895c0UL, // cam_render_scene
+        },
+    },
+};
+
+bool DoesSignatureMatch(const ExeSignature *sig) {
+    HANDLE hProcess = GetCurrentProcess();
+    HMODULE hModule = GetModuleHandle(NULL);
+    DWORD baseAddress = (DWORD)hModule;
+    // Read memory
+    UCHAR mem[ExeSignatureBytesMax];
+    SIZE_T bytesRead = 0;
+    bool ok = ReadProcessMemory(hProcess,
+        (LPCVOID)(baseAddress+sig->offset), mem, ExeSignatureBytesMax, &bytesRead);
+    if (!ok || bytesRead != ExeSignatureBytesMax) return false;
+    // Apply fixups
+    for (int i=0; i<ExeSignatureFixupsMax; ++i) {
+        CHAR o = sig->fixups[i];
+        if (o < 0) break;
+        if (o >= (ExeSignatureBytesMax - sizeof(DWORD))) {
+#ifndef NDEBUG
+            printf("Fixup outwith signature bounds.\n");
+#endif
+            return false;
+        }
+        // Subtract base address
+        DWORD v;
+        memcpy(&v, &mem[o], sizeof(DWORD));
+        v -= baseAddress;
+        memcpy(&mem[o], &v, sizeof(DWORD));
+    }
+    // Compare the signature
+    if (sig->size > ExeSignatureBytesMax) {
+#ifndef NDEBUG
+        printf("Signature size too large.\n");
+#endif
+        return false;
+    }
+    int r = memcmp(mem, sig->bytes, sig->size);
+    return (r == 0);
+}
+
+ExeIdentity IdentifyExe() {
+    for (int i=0; i<ExeIdentityCount; ++i) {
+        const ExeSignature *sig = &(ExeInfoTable[i].signature);
+        if (DoesSignatureMatch(sig)) {
+            return static_cast<ExeIdentity>(i);
+        }
+    }
+    return ExeIdentityUnknown;
+}
+
 /*** Script class declarations (this will usually be in a header file) ***/
 
 class cScr_PeriaptControl : public cScript
@@ -82,134 +224,6 @@ const unsigned int cScriptModule::sm_ScriptsArraySize = sizeof(sm_ScriptsArray)/
 
 /* -------------------------------------------------------- */
 
-#define ExeSignatureBytesMax 32
-#define ExeSignatureFixupsMax 8
-enum ExeIdentity {
-    ExeThief_v126 = 0,
-    ExeDromEd_v126 = 1,
-    ExeThief_v127 = 2,
-    ExeDromEd_v127 = 3,
-
-    ExeIdentityCount,
-    ExeIdentityUnknown = -1,
-};
-struct ExeSignature {
-    DWORD offset;
-    DWORD size;
-    UCHAR bytes[ExeSignatureBytesMax];
-    CHAR fixups[ExeSignatureFixupsMax];
-};
-typedef void (*FnPtr)(void);
-struct ExeFunctionPointers {
-    FnPtr cam_render_scene;
-};
-struct ExeInfo {
-    const char *name;
-    const char *version;
-    ExeSignature signature;
-    ExeFunctionPointers functions;
-};
-static const ExeInfo ExeInfoTable[ExeIdentityCount] = {
-    {
-        "Thief2", "ND 1.26",
-        {   // Signature
-            0x001bc7a0UL, 31,
-            {
-            0x83,0xec,0x28,0xdd,0x05,0xd8,0xde,0x38,
-            0x00,0x53,0x56,0xdd,0x54,0x24,0x24,0xdd,
-            0x05,0xe0,0xde,0x38,0x00,0x8b,0xf0,0x8b,
-            0x06,0xdd,0x54,0x24,0x1c,0xd9,0x05,
-            },
-            { 5, 17, -1 },
-        },
-    },
-    {
-        "DromEd", "ND 1.26",
-        {   // Signature
-            0x00286960UL, 31,
-            {
-                0x83,0xec,0x28,0xdd,0x05,0xa8,0xdf,0x4a,
-                0x00,0x53,0x56,0xdd,0x54,0x24,0x24,0xdd,
-                0x05,0xb0,0xdf,0x4a,0x00,0x8b,0xf0,0x8b,
-                0x06,0xdd,0x54,0x24,0x1c,0xd9,0x05,
-            },
-            { 5, 17, -1 },
-        },
-    },
-    {
-        "Thief2", "ND 1.27",
-        {   // Signature
-            0x001bd820UL, 31,
-            {
-                0x83,0xec,0x28,0xdd,0x05,0xd8,0xee,0x38,
-                0x00,0x53,0x56,0xdd,0x54,0x24,0x24,0xdd,
-                0x05,0xe0,0xee,0x38,0x00,0x8b,0xf0,0x8b,
-                0x06,0xdd,0x54,0x24,0x1c,0xd9,0x05,
-            },
-            { 5, 17, -1 },
-        },
-    },
-    {
-        "DromEd", "ND 1.27",
-        {   // Signature
-            0x002895c0UL, 31,
-            {
-                0x83,0xec,0x28,0xdd,0x05,0xa8,0x1f,0x4b,
-                0x00,0x53,0x56,0xdd,0x54,0x24,0x24,0xdd,
-                0x05,0xb0,0x1f,0x4b,0x00,0x8b,0xf0,0x8b,
-                0x06,0xdd,0x54,0x24,0x1c,0xd9,0x05,
-            },
-            { 5, 17, -1 },
-        },
-    },
-};
-
-BOOL DoesSignatureMatch(const ExeSignature *sig) {
-    HANDLE hProcess = GetCurrentProcess();
-    HMODULE hModule = GetModuleHandle(NULL);
-    DWORD baseAddress = (DWORD)hModule;
-    // Read memory
-    UCHAR mem[ExeSignatureBytesMax];
-    SIZE_T bytesRead = 0;
-    BOOL ok = ReadProcessMemory(hProcess,
-        (LPCVOID)(baseAddress+sig->offset), mem, ExeSignatureBytesMax, &bytesRead);
-    if (!ok || bytesRead != ExeSignatureBytesMax) return false;
-    // Apply fixups
-    for (int i=0; i<ExeSignatureFixupsMax; ++i) {
-        CHAR o = sig->fixups[i];
-        if (o < 0) break;
-        if (o >= (ExeSignatureBytesMax - sizeof(DWORD))) {
-#ifndef NDEBUG
-            printf("Fixup outwith signature bounds.\n");
-#endif
-            return false;
-        }
-        // Subtract base address
-        DWORD v;
-        memcpy(&v, &mem[o], sizeof(DWORD));
-        v -= baseAddress;
-        memcpy(&mem[o], &v, sizeof(DWORD));
-    }
-    // Compare the signature
-    if (sig->size > ExeSignatureBytesMax) {
-#ifndef NDEBUG
-        printf("Signature size too large.\n");
-#endif
-        return false;
-    }
-    int r = memcmp(mem, sig->bytes, sig->size);
-    return (r == 0);
-}
-
-ExeIdentity IdentifyExe() {
-    for (int i=0; i<ExeIdentityCount; ++i) {
-        const ExeSignature *sig = &(ExeInfoTable[i].signature);
-        if (DoesSignatureMatch(sig)) {
-            return static_cast<ExeIdentity>(i);
-        }
-    }
-    return ExeIdentityUnknown;
-}
 
 static void printLastError()
 {
@@ -248,22 +262,21 @@ void readMem(DWORD offset, DWORD len)
     }
 }
 
-
-/* -------------------------------------------------------- */
-
+/*** Function patching ***/
 
 #include <stdio.h>
+#include <windows.h>
 
 #define PREFIX "periapt: "
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
 {
-    static BOOL didAllocConsole = false;
+    static bool didAllocConsole = false;
     switch (reason) {
     case DLL_PROCESS_ATTACH: {
         // Try to identify this exe.
         ExeIdentity identity = IdentifyExe();
-        BOOL isEditor, isIdentified;
+        bool isEditor, isIdentified;
         switch (identity) {
         case ExeThief_v126:
         case ExeThief_v127:
@@ -281,13 +294,24 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
             break;
         }
         // Decide accordingly how to handle console output.
-        BOOL shouldAllocConsole = (!isEditor || !isIdentified);
-        BOOL shouldRedirectStdout = (isEditor || shouldAllocConsole);
+        bool shouldAllocConsole = (!isEditor || !isIdentified);
+        bool shouldRedirectStdout = (isEditor || shouldAllocConsole);
         if (shouldAllocConsole) {
-            didAllocConsole = (BOOL)AllocConsole();
+            didAllocConsole = AllocConsole();
         }
         if (shouldRedirectStdout) {
             freopen("CONOUT$", "w", stdout);
+
+            // FIXME: for my convenience, let's put the console in
+            //        a handy location.
+            HWND hwnd = GetConsoleWindow();
+            RECT rect;
+            GetWindowRect(hwnd, &rect);
+            int left = 1930;
+            int top = 480;
+            int width = (int)(rect.right - rect.left);
+            int height = (int)(rect.bottom - rect.top);
+            MoveWindow(hwnd, left, top, width, height, TRUE);
         }
         // Now that we (hopefully) have somewhere for output to go,
         // we can print some useful messages.
@@ -298,15 +322,12 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
             const ExeInfo *info = &ExeInfoTable[identity];
             printf(PREFIX "Identified exe as %s %s (%s)\n",
                 info->name, info->version, (isEditor ? "EDITOR" : "GAME"));
+            // FIXME: remove this:
+            readMem(info->functions.cam_render_scene, 0x20);
         } else {
             printf(PREFIX "Cannot identify exe; must not continue!\n");
             return false;
         }
-
-        // # 0x286960 - _cam_render_scene in Dromed ND 1.26
-        // # 0x1bc7a0 - _cam_render_scene in Thief2 ND 1.26
-        // # 0x1bd820 - _cam_render_scene in Thief2 ND 1.27
-        readMem(0x286960, 0x20);
     } break;
     case DLL_PROCESS_DETACH: {
         printf(PREFIX "DLL_PROCESS_DETACH\n");
