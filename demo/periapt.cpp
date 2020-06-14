1165456e72e5349a35b3178ffcc2925b95a28c52
Andy Durdin <me@andy.durdin.net>
2020-06-14 01:54:34 +0100

    Start working towards usable structure.

--- demo/periapt.cpp
+++ demo/periapt.cpp
@@ -35,11 +35,11 @@ using namespace std;
 
 /*** Script class declarations (this will usually be in a header file) ***/
 
-class cScr_Readmem : public cScript
+class cScr_PeriaptControl : public cScript
 {
 public:
-    virtual ~cScr_Readmem() { }
-    cScr_Readmem(const char* pszName, int iHostObjId)
+    virtual ~cScr_PeriaptControl() { }
+    cScr_PeriaptControl(const char* pszName, int iHostObjId)
         : cScript(pszName,iHostObjId)
     { }
 
@@ -51,49 +51,12 @@ public:
 
 /*** Script implementations ***/
 
-long cScr_Readmem::ReceiveMessage(sScrMsg* pMsg, sMultiParm* pReply, eScrTraceAction eTrace)
+long cScr_PeriaptControl::ReceiveMessage(sScrMsg* pMsg, sMultiParm* pReply, eScrTraceAction eTrace)
 {
     long iRet = cScript::ReceiveMessage(pMsg, pReply, eTrace);
 
-    try
-    {
-        string sOutput = pMsg->message;
-        sOutput += "(";
-        sOutput += static_cast<const char*>(pMsg->data);
-        sOutput += ",";
-        sOutput += static_cast<const char*>(pMsg->data2);
-        sOutput += ",";
-        sOutput += static_cast<const char*>(pMsg->data2);
-        sOutput += ")";
-
-        int iTime = -1001;
-        IPropertySrv* pProps = static_cast<IPropertySrv*>(g_pScriptManager->GetService(IID_IPropertyScriptService));
-        if (pProps)
-        {
-            if (pProps->Possessed(m_iObjId, "ScriptTiming"))
-            {
-                cMultiParm mpTiming;
-                pProps->Get(mpTiming, m_iObjId, "ScriptTiming", NULL);
-                if (static_cast<int>(mpTiming) > 0)
-                {
-                    iTime = mpTiming;
-                }
-            }
-            pProps->Release();
-        }
-
-        IDarkUISrv* pUI = static_cast<IDarkUISrv*>(g_pScriptManager->GetService(IID_IDarkUIScriptService));
-        if (pUI)
-        {
-            pUI->TextMessage(sOutput.c_str(), 0, iTime);
-            pUI->Release();
-        }
-    }
-    catch (exception& err)
-    {
-        // Don't pass exceptions out of the module.
-        if (g_pfnMPrintf)
-            g_pfnMPrintf("Error! %s\n", err.what());
+    if (stricmp(pMsg->message, "TurnOn") == 0) {
+    } else if (stricmp(pMsg->message, "TurnOff") == 0) {
     }
 
     return iRet;
@@ -101,18 +64,18 @@ long cScr_Readmem::ReceiveMessage(sScrMsg* pMsg, sMultiParm* pReply, eScrTraceAc
 
 /*** Script Factories ***/
 
-IScript* cScr_Readmem::ScriptFactory(const char* pszName, int iHostObjId)
+IScript* cScr_PeriaptControl::ScriptFactory(const char* pszName, int iHostObjId)
 {
-    if (stricmp(pszName,"Readmem") != 0)
+    if (stricmp(pszName,"PeriaptControl") != 0)
         return NULL;
 
     // Use a static string, so I don't have to make a copy.
-    cScr_Readmem* pscrRet = new(nothrow) cScr_Readmem("Readmem", iHostObjId);
+    cScr_PeriaptControl* pscrRet = new(nothrow) cScr_PeriaptControl("PeriaptControl", iHostObjId);
     return static_cast<IScript*>(pscrRet);
 }
 
 const sScrClassDesc cScriptModule::sm_ScriptsArray[] = {
-    { "periapt", "Readmem", "CustomScript", cScr_Readmem::ScriptFactory },
+    { "periapt", "PeriaptControl", "CustomScript", cScr_PeriaptControl::ScriptFactory },
 };
 const unsigned int cScriptModule::sm_ScriptsArraySize = sizeof(sm_ScriptsArray)/sizeof(sm_ScriptsArray[0]);
 
@@ -121,62 +84,85 @@ const unsigned int cScriptModule::sm_ScriptsArraySize = sizeof(sm_ScriptsArray)/
 
 #define ExeSignatureBytesMax 32
 #define ExeSignatureFixupsMax 8
-enum ExeType {
-    ExeTypeGame = 0,
-    ExeTypeEditor = 1,
+enum ExeIdentity {
+    ExeThief_v126 = 0,
+    ExeDromEd_v126 = 1,
+    ExeThief_v127 = 2,
+    ExeDromEd_v127 = 3,
+
+    ExeIdentityCount,
+    ExeIdentityUnknown = -1,
 };
 struct ExeSignature {
-    ExeType type;
-    const char *name;
-    const char *version;
-    DWORD relativeAddress;
+    DWORD offset;
     DWORD size;
     UCHAR bytes[ExeSignatureBytesMax];
     CHAR fixups[ExeSignatureFixupsMax];
 };
-static const ExeSignature ExeSignatures[] = {
+typedef void (*FnPtr)(void);
+struct ExeFunctionPointers {
+    FnPtr cam_render_scene;
+};
+struct ExeInfo {
+    const char *name;
+    const char *version;
+    ExeSignature signature;
+    ExeFunctionPointers functions;
+};
+static const ExeInfo ExeInfoTable[ExeIdentityCount] = {
     {
-        ExeTypeEditor, "DromEd", "ND 1.26", 0x00286960UL, 31,
-        {
-            0x83,0xec,0x28,0xdd,0x05,0xa8,0xdf,0x4a,
-            0x00,0x53,0x56,0xdd,0x54,0x24,0x24,0xdd,
-            0x05,0xb0,0xdf,0x4a,0x00,0x8b,0xf0,0x8b,
-            0x06,0xdd,0x54,0x24,0x1c,0xd9,0x05,
-        },
-        { 5, 17, -1 },
-    },
-    {
-        ExeTypeGame, "Thief2", "ND 1.26", 0x001bc7a0UL, 31,
-        {
+        "Thief2", "ND 1.26",
+        {   // Signature
+            0x001bc7a0UL, 31,
+            {
             0x83,0xec,0x28,0xdd,0x05,0xd8,0xde,0x38,
             0x00,0x53,0x56,0xdd,0x54,0x24,0x24,0xdd,
             0x05,0xe0,0xde,0x38,0x00,0x8b,0xf0,0x8b,
             0x06,0xdd,0x54,0x24,0x1c,0xd9,0x05,
+            },
+            { 5, 17, -1 },
         },
-        { 5, 17, -1 },
     },
     {
-        ExeTypeEditor, "DromEd", "ND 1.27", 0x002895c0UL, 31,
-        {
-            0x83,0xec,0x28,0xdd,0x05,0xa8,0x1f,0x4b,
-            0x00,0x53,0x56,0xdd,0x54,0x24,0x24,0xdd,
-            0x05,0xb0,0x1f,0x4b,0x00,0x8b,0xf0,0x8b,
-            0x06,0xdd,0x54,0x24,0x1c,0xd9,0x05,
+        "DromEd", "ND 1.26",
+        {   // Signature
+            0x00286960UL, 31,
+            {
+                0x83,0xec,0x28,0xdd,0x05,0xa8,0xdf,0x4a,
+                0x00,0x53,0x56,0xdd,0x54,0x24,0x24,0xdd,
+                0x05,0xb0,0xdf,0x4a,0x00,0x8b,0xf0,0x8b,
+                0x06,0xdd,0x54,0x24,0x1c,0xd9,0x05,
+            },
+            { 5, 17, -1 },
         },
-        { 5, 17, -1 },
     },
     {
-        ExeTypeGame, "Thief2", "ND 1.27", 0x001bd820UL, 31,
-        {
-            0x83,0xec,0x28,0xdd,0x05,0xd8,0xee,0x38,
-            0x00,0x53,0x56,0xdd,0x54,0x24,0x24,0xdd,
-            0x05,0xe0,0xee,0x38,0x00,0x8b,0xf0,0x8b,
-            0x06,0xdd,0x54,0x24,0x1c,0xd9,0x05,
+        "Thief2", "ND 1.27",
+        {   // Signature
+            0x001bd820UL, 31,
+            {
+                0x83,0xec,0x28,0xdd,0x05,0xd8,0xee,0x38,
+                0x00,0x53,0x56,0xdd,0x54,0x24,0x24,0xdd,
+                0x05,0xe0,0xee,0x38,0x00,0x8b,0xf0,0x8b,
+                0x06,0xdd,0x54,0x24,0x1c,0xd9,0x05,
+            },
+            { 5, 17, -1 },
         },
-        { 5, 17, -1 },
+    },
+    {
+        "DromEd", "ND 1.27",
+        {   // Signature
+            0x002895c0UL, 31,
+            {
+                0x83,0xec,0x28,0xdd,0x05,0xa8,0x1f,0x4b,
+                0x00,0x53,0x56,0xdd,0x54,0x24,0x24,0xdd,
+                0x05,0xb0,0x1f,0x4b,0x00,0x8b,0xf0,0x8b,
+                0x06,0xdd,0x54,0x24,0x1c,0xd9,0x05,
+            },
+            { 5, 17, -1 },
+        },
     },
 };
-const int ExeSignatureCount = sizeof(ExeSignatures)/sizeof(ExeSignatures[0]);
 
 BOOL DoesSignatureMatch(const ExeSignature *sig) {
     HANDLE hProcess = GetCurrentProcess();
@@ -186,7 +172,7 @@ BOOL DoesSignatureMatch(const ExeSignature *sig) {
     UCHAR mem[ExeSignatureBytesMax];
     SIZE_T bytesRead = 0;
     BOOL ok = ReadProcessMemory(hProcess,
-        (LPCVOID)(baseAddress+sig->relativeAddress), mem, ExeSignatureBytesMax, &bytesRead);
+        (LPCVOID)(baseAddress+sig->offset), mem, ExeSignatureBytesMax, &bytesRead);
     if (!ok || bytesRead != ExeSignatureBytesMax) return false;
     // Apply fixups
     for (int i=0; i<ExeSignatureFixupsMax; ++i) {
@@ -215,19 +201,14 @@ BOOL DoesSignatureMatch(const ExeSignature *sig) {
     return (r == 0);
 }
 
-BOOL IdentifyExe(ExeType *type, const char **name, const char **version) {
-    BOOL foundMatch = false;
-    for (int i=0; i<ExeSignatureCount; ++i) {
-        const ExeSignature *sig = &ExeSignatures[i];
-        BOOL matches = DoesSignatureMatch(sig);
-        if (matches) {
-            *type = sig->type;
-            *name = sig->name;
-            *version = sig->version;
-            return true;
+ExeIdentity IdentifyExe() {
+    for (int i=0; i<ExeIdentityCount; ++i) {
+        const ExeSignature *sig = &(ExeInfoTable[i].signature);
+        if (DoesSignatureMatch(sig)) {
+            return static_cast<ExeIdentity>(i);
         }
     }
-    return false;
+    return ExeIdentityUnknown;
 }
 
 static void printLastError()
@@ -281,11 +262,24 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
     switch (reason) {
     case DLL_PROCESS_ATTACH: {
         // Try to identify this exe.
-        ExeType type;
-        const char *name;
-        const char *version;
-        BOOL isIdentified = IdentifyExe(&type, &name, &version);
-        BOOL isEditor = (type == ExeTypeEditor);
+        ExeIdentity identity = IdentifyExe();
+        BOOL isEditor, isIdentified;
+        switch (identity) {
+        case ExeThief_v126:
+        case ExeThief_v127:
+            isEditor = false;
+            isIdentified = true;
+            break;
+        case ExeDromEd_v126:
+        case ExeDromEd_v127:
+            isEditor = true;
+            isIdentified = true;
+            break;
+        default:
+            isEditor = false;
+            isIdentified = false;
+            break;
+        }
         // Decide accordingly how to handle console output.
         BOOL shouldAllocConsole = (!isEditor || !isIdentified);
         BOOL shouldRedirectStdout = (isEditor || shouldAllocConsole);
@@ -301,8 +295,9 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
         printf(PREFIX "Base address: 0x%X\n", (unsigned long)GetModuleHandle(NULL));
         // Display the results of identifying the exe.
         if (isIdentified) {
+            const ExeInfo *info = &ExeInfoTable[identity];
             printf(PREFIX "Identified exe as %s %s (%s)\n",
-                name, version, (type == ExeTypeEditor ? "EDITOR" : "GAME"));
+                info->name, info->version, (isEditor ? "EDITOR" : "GAME"));
         } else {
             printf(PREFIX "Cannot identify exe; must not continue!\n");
             return false;
